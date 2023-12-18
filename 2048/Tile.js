f51fe21e2eee65c73ea07fccd8355b3d413a129f
wdhere <wdhere@163.com>
2023-12-18 15:25:05 +0800

    fix: typo

--- 2048/Tile.js
+++ 2048/Tile.js
@@ -4,7 +4,7 @@ export default class Tile {
   #y;
   #value;
 
-  constructor(tileContainer, value = Math.random() > 0.5 ? 512 : 2048) {
+  constructor(tileContainer, value = Math.random() > 0.5 ? 2 : 4) {
     this.#tileElement = document.createElement("div");
     this.#tileElement.classList.add("tile");
     tileContainer.append(this.#tileElement);
